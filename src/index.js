2102ac3af210e196b87a10660d240f82788b6edf
jperram92 <93854423+jperram92@users.noreply.github.com>
2025-03-08 21:42:58 +1100

    added a public index method

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';  // Optional, but commonly included for global styles
-import App from './App';  // Main App component
+import App from './App.js';  // Main App component
 import reportWebVitals from './reportWebVitals';  // Optional, for measuring performance
 
 ReactDOM.render(
