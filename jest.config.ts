d467843df0af3be7edd86cf11874b5d331dbffd8
jperram92 <93854423+jperram92@users.noreply.github.com>
2025-03-08 21:15:39 +1100

    Update main yaml

--- jest.config.ts
+++ jest.config.ts
@@ -1,22 +1,17 @@
 module.exports = {
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",  // Adjust the alias for your project structure
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',  // Use ts-jest for TypeScript files
-    '^.+\\.(js|jsx)$': 'babel-jest',  // Use babel-jest for JavaScript files
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
   },
   globals: {
     'ts-jest': {
-      tsconfig: {
-        jsx: 'react-jsx', // For JSX handling
-        sourceMap: true,
-        module: 'ESNext',
-        target: 'ES2020',
-      },
+      tsconfig: './tsconfig.jest.json',  // Specify the new tsconfig.jest.json file
     },
   },
-  testEnvironment: 'jsdom',  // Required for testing in a browser-like environment
-  preset: 'ts-jest',  // Set ts-jest as the preset
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],  // Handle the appropriate extensions
+  testEnvironment: 'jsdom',
+  preset: 'ts-jest',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
